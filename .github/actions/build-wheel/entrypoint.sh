aee1061fc893e5b95eed9f467f62c0c311475385
Huanchen Zhai <hczhai@ucla.edu>
2021-09-27 21:24:36 -0700

    add workflow actions

---
+++ .github/actions/build-wheel/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e -x
+
+cd /github/workspace
+
+PYTHON_VERSION=$1
+if [ "${PYTHON_VERSION}" = "3.6" ]; then
+    PY_VER=cp36-cp36m
+elif [ "${PYTHON_VERSION}" = "3.7" ]; then
+    PY_VER=cp37-cp37m
+elif [ "${PYTHON_VERSION}" = "3.8" ]; then
+    PY_VER=cp38-cp38
+elif [ "${PYTHON_VERSION}" = "3.9" ]; then
+    PY_VER=cp39-cp39
+elif [ "${PYTHON_VERSION}" = "3.10" ]; then
+    PY_VER=cp310-cp310
+fi
+
+PY_EXE=/opt/python/"${PY_VER}"/bin/python3
+sed -i "/DPYTHON_EXECUTABLE/a \                '-DPYTHON_EXECUTABLE=${PY_EXE}'," setup.py
+
+/opt/python/"${PY_VER}"/bin/pip install --upgrade --no-cache-dir pip
+/opt/python/"${PY_VER}"/bin/pip install --no-cache-dir mkl==2019 mkl-include intel-openmp numpy cmake==3.17 pybind11
+/opt/python/"${PY_VER}"/bin/pip wheel . -w ./dist --no-deps
+
+find . -type f -iname "*-linux*.whl" -exec sh -c "auditwheel repair '{}' -w \$(dirname '{}') --plat '${PLAT}'" \;
+find . -type f -iname "*-manylinux*.whl"
