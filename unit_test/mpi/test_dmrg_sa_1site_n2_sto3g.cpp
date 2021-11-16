91912df87396ec9b294feeed64f98bc1bd19ced2
Huanchen Zhai <hczhai@ucla.edu>
2021-11-16 03:18:10 -0800

    fix sa test

--- unit_test/mpi/test_dmrg_sa_1site_n2_sto3g.cpp
+++ unit_test/mpi/test_dmrg_sa_1site_n2_sto3g.cpp
@@ -133,6 +133,7 @@ void TestOneSiteDMRGN2STO3GSA::test_dmrg(
     shared_ptr<DMRG<S, FL, FL>> dmrg =
         make_shared<DMRG<S, FL, FL>>(me, bdims, noises);
     dmrg->iprint = 2;
+    dmrg->davidson_soft_max_iter = 2000;
     dmrg->noise_type = NoiseTypes::ReducedPerturbativeCollected;
     dmrg->trunc_type = dmrg->trunc_type | TruncationTypes::RealDensityMatrix;
     double energy = dmrg->solve(10, mps->center == 0, 1E-8);
