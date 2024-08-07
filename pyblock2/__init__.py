1acc274f7f173270cdc74279d5efa99f5bf9f3b7
Huanchen Zhai <hczhai@ucla.edu>
2024-08-06 22:52:19 -0700

    fix windows rel path in env:path

--- pyblock2/__init__.py
+++ pyblock2/__init__.py
@@ -7,5 +7,5 @@ sys.path += [os.path.join(os.path.dirname(
 
 if os.name == "nt" and sys.version_info[1] >= 8:
     for path in os.environ["PATH"].split(";"):
-        if path != "" and os.path.exists(path):
-            os.add_dll_directory(path)
+        if path != "" and os.path.exists(os.path.abspath(path)):
+            os.add_dll_directory(os.path.abspath(path))
